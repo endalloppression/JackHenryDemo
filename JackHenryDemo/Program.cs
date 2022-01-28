4ccfc24a29e62d940fd9103ba36e157cfdc89eca
endalloppression <endalloppression@protonmail.com>
2022-01-27 21:45:35 -0600

    Add project files.

---
+++ JackHenryDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace JackHenryDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Staring Twitter Sample Stream V2...");
+            var tweetCount = 0;
+            await Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    tweetCount = await new TwitterFactory().GetTwitterStreamAsync(new TwitterClient(args[0], args[1], args[2]));
+
+                    Console.WriteLine("\t Total estimated number of tweets: " + tweetCount);
+                    Console.WriteLine("\t Estimated rate of tweets over a 60 second time period: " + tweetCount / 2);
+                    Console.WriteLine("\t Stopping Twitter Sample Stream V2... \n");
+                }
+                catch (NullReferenceException tweet)
+                {
+                    Console.WriteLine(tweet.Message.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+
+            });
+        }
+    }
+}
---
+++ JackHenryDemo/TwitterFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace JackHenryDemo
+{
+    internal class TwitterFactory
+    {
+        public async Task<int> GetTwitterStreamAsync(TwitterClient appClient)
+        {
+            var sampleStreamV2 = appClient.StreamsV2.CreateSampleStream();
+            var tweetCount = 0;
+            sampleStreamV2.TweetReceived += (sender, args) =>
+            {
+                try
+                {
+                    Console.WriteLine("Author ID: " + args.Tweet.AuthorId);
+                    Console.WriteLine("Tweet ID: " + args.Tweet.Id);
+                    Console.WriteLine("Source: " + args.Tweet.Source);
+                    Console.WriteLine("Created At: " + args.Tweet.CreatedAt);
+                    Console.WriteLine("Language: " + args.Tweet.Lang);
+                    Console.WriteLine("Tweet: " + args.Tweet.Text + "\n");
+                    tweetCount++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            };
+            sampleStreamV2.StartAsync().Wait(120000);
+            return tweetCount;
+        }
+    }
+}
